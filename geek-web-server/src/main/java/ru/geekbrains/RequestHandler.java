cf2049ddef8f9b2fa2bc08ec36ec14bc851b9bad
alapshov <lapshov.av@mail.ru>
2022-08-14 11:42:54 +0300

    Learn_2

--- geek-web-server/src/main/java/ru/geekbrains/RequestHandler.java
+++ geek-web-server/src/main/java/ru/geekbrains/RequestHandler.java
@@ -1,34 +1,49 @@
 package ru.geekbrains;
 
-import java.io.*;
-import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import ru.geekbrains.service.FileService;
+import ru.geekbrains.service.SocketService;
+
+import java.io.IOException;
+import java.util.Deque;
 
 public class RequestHandler implements Runnable {
 
-    private final Socket socket;
+    private final SocketService socketService;
 
-    private final String folder;
+    private final FileService fileService;
 
-    public RequestHandler(Socket socket, String folder) {
-        this.socket = socket;
-        this.folder = folder;
+    public RequestHandler(SocketService socketService, FileService fileService) {
+        this.socketService = socketService;
+        this.fileService = fileService;
     }
 
     @Override
     public void run() {
-        try (BufferedReader input = new BufferedReader(
-                new InputStreamReader(
-                        socket.getInputStream(), StandardCharsets.UTF_8));
-             PrintWriter output = new PrintWriter(socket.getOutputStream())
-        ) {
-            String[] parts = new Handler().getPartsReadLine(input);
-            new ru.geekbrains.Files().fileRead(folder, parts, output);
-        } catch (IOException e) {
-            e.printStackTrace();
+        Deque<String> rawRequest = socketService.readRequest();
+        String firstLine = rawRequest.pollFirst();
+        String[] parts = firstLine.split(" ");
+
+        if (!fileService.exists(parts[1])) {
+            String rawResponse =
+                    "HTTP/1.1 404 NOT_FOUND\n" +
+                    "Content-Type: text/html; charset=utf-8\n" +
+                    "\n" +
+                    "<h1>Файл не найден!</h1>";
+            socketService.writeResponse(rawResponse);
+            return;
         }
+
+        String rawResponse = "HTTP/1.1 200 OK\n" +
+                "Content-Type: text/html; charset=utf-8\n" +
+                "\n" +
+                fileService.readFile(parts[1]);
+        socketService.writeResponse(rawResponse);
+
+        try {
+            socketService.close();
+        } catch (IOException ex) {
+            throw new IllegalStateException(ex);
+        }
+        System.out.println("Client disconnected!");
     }
 }
--- geek-web-server/src/main/java/ru/geekbrains/WebServer.java
+++ geek-web-server/src/main/java/ru/geekbrains/WebServer.java
@@ -1,12 +1,15 @@
 package ru.geekbrains;
 
+import ru.geekbrains.service.FileService;
+import ru.geekbrains.service.SocketService;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class WebServer {
 
-    private static String WWW = "www";
+    private static String WWW = "/Users/aleks/dev/geek-architecture-02/www";
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(8088)) {
@@ -16,7 +19,7 @@ public class WebServer {
                 Socket socket = serverSocket.accept();
                 System.out.println("New client connected!");
 
-                new Thread(new RequestHandler(socket, WWW)).start();
+                new Thread(new RequestHandler(new SocketService(socket), new FileService(WWW))).start();
             }
         } catch (IOException e) {
             e.printStackTrace();
